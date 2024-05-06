92dbe7b9b839a314ed35f033e94f785f6ef64190
SebastianRiveros <sriveros@unsa.edu.pe>
2024-05-05 22:33:43 -0500

    Implementacion del metodo add, compilacion del archivo grupo1.java, que genera el archivo Calculator.class, cambio en indentacion

--- grupo1.java
+++ grupo1.java
@@ -1,7 +1,14 @@
 class Calculator {
-  int add(int a, int b){ return 0; }
-  int sub(int a, int b){ return 0; }
-  int mul(int a, int b){ return 0; }
-  int div(int a, int b){ return 0; }
-  int mod(int a, int b){ return 0; }
+	//Metodos de la calcualdora
+	int add(int a, int b){ return a+b; }
+	int sub(int a, int b){ return 0; }
+	int mul(int a, int b){ return 0; }
+	int div(int a, int b){ return 0; }
+	int mod(int a, int b){ return 0; }
+	
+	//Pruebas
+	public static void main(String[] args){
+		Calculator c1 = new Calculator();
+		System.out.println(c1.add(10,10));
+	}
 }
